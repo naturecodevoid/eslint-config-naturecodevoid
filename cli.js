7fd56bb5967ee7b1803f5b506fc8cfbe920f651d
naturecodevoid <44983869+naturecodevoid@users.noreply.github.com>
2020-05-26 09:45:07 -0700

    Add postinstall and remove log

--- cli.js
+++ cli.js
@@ -3,4 +3,3 @@ require("fs").copyFileSync(
     require("path").resolve(__dirname, "exports.js"),
     require("path").resolve(process.cwd(), ".eslintrc.js"),
 );
-console.log("Please run 'npm i -D eslint-config-naturecodevoid' to finish installing, if you haven't already.");
